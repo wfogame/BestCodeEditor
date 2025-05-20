c23b281d6feeed1b192419e1ecf4bccc812c8561
wfogame <1740712@fcpsschools.net>
2025-05-19 22:15:03 -0400

    further format with prettier and added storyreadme which gives cool story

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is a totally 100% Medium-WEight text editor with PREBUILT place to test all ur nesccary thingies
+Click story.readme.md IDK why file but just do ( me getting -1  views :OOOO)
--- server.js
+++ server.js
@@ -19,61 +19,46 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-
-  fs.readFile(path.join(__dirname, pathname.slice(1)),(err,data)=>{
+  fs.readFile(path.join(__dirname, pathname.slice(1)), (err, data) => {
     let ContentType;
- 
 
-
-switch (PathExt) {
-        case ".html":
-            ContentType = "text/html";
-            break;
-        case ".css":
-            ContentType = "text/css";
-            break;
-        case ".js":
-            ContentType = "application/javascript";
-            break;
-        case ".json":
-            ContentType = "application/json";
-            break;
-        case ".png":
-            ContentType = "image/png";
-            break;
-        case ".jpg":
-        case ".jpeg":
-            ContentType = "image/jpeg";
-            break;
-        case ".svg":
-            ContentType = "image/svg+xml";
-            break;
-        case ".gif":
-            ContentType = "image/gif";
-            break;
-        case ".txt":
-            ContentType = "text/plain";
-            break;
-        default:
-            ContentType = "application/octet-stream";
+    switch (PathExt) {
+      case ".html":
+        ContentType = "text/html";
+        break;
+      case ".css":
+        ContentType = "text/css";
+        break;
+      case ".js":
+        ContentType = "application/javascript";
+        break;
+      case ".json":
+        ContentType = "application/json";
+        break;
+      case ".png":
+        ContentType = "image/png";
+        break;
+      case ".jpg":
+      case ".jpeg":
+        ContentType = "image/jpeg";
+        break;
+      case ".svg":
+        ContentType = "image/svg+xml";
+        break;
+      case ".gif":
+        ContentType = "image/gif";
+        break;
+      case ".txt":
+        ContentType = "text/plain";
+        break;
+      default:
+        ContentType = "application/octet-stream";
     }
 
-
-
-
     res.statusCode = 200;
-    res.setHeader("Content-Type",ContentType);
+    res.setHeader("Content-Type", ContentType);
     res.end(data);
-
-
-  })
-
-
-
- 
-
-
-
+  });
 });
 
 server.listen(3000, () => {
